905302d2035e02c41779639b421ff4bf984c46e6
deniseuan <99851467+deniseuan@users.noreply.github.com>
2024-05-28 14:26:57 -0600

    End of section 8

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -18,6 +18,10 @@ namespace API.Extensions
 
             services.AddCors();
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRespository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            
+            
             return services;
         }
     }
