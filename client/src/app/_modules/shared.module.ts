b58465a79089f8435de2602f5c39932052d548ef
deniseuan <99851467+deniseuan@users.noreply.github.com>
2024-05-29 12:43:37 -0600

    End of section 10

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,6 +3,7 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 
 
@@ -16,12 +17,16 @@ import { ToastrModule } from 'ngx-toastr';
       {
         positionClass: 'toast-bottom-right'
       }
-    ) // ToastrModule added
+    ), // ToastrModule added
+    NgxSpinnerModule.forRoot({
+      type: 'timer'
+    })
   ],
   exports: [
     BsDropdownModule,
     ToastrModule,
-    TabsModule
+    TabsModule,
+    NgxSpinnerModule
   ]
 })
 export class SharedModule { }
